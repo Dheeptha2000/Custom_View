f4040d56a40fc2e8c14c7f489b3ac6ad90e05178
Ramesh <dheeptha2000@gmail.com>
2020-04-05 19:08:02 +0530

    Initial commit

---
+++ app/src/main/java/com/dheeptha/customview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.dheeptha.customview;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button ClickMe;
+    private ShapeSelectorView shapeSelectorView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ClickMe = findViewById(R.id.button_click_me);
+        shapeSelectorView = findViewById(R.id.ShapeSelectorView_selector);
+
+        ClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowMessage();
+            }
+        });
+
+    }
+
+    private void ShowMessage() {
+        Toast.makeText(this,shapeSelectorView.getSelectedShape(),Toast.LENGTH_LONG).show();
+    }
+}
